ad3b067618d6edd39ac65f8a63eb894c0cce8855
NataliyaChe <happylibrarygeek@gmail.com>
2022-07-02 10:06:00 +0300

    First commit.

---
+++ js-lesson_01/index.js
@@ -0,0 +1,26 @@
+
+
+
+let yourName = prompt('Ваше имя?');
+alert(`Hello, ${yourName}`); 
+
+/* 
+Варианты записи переменных:
+let myName = 'Nata';
+*/
+
+// myName = 'Nata';
+
+// var myName = 'Nata'; 
+
+// const myName = 'Nataliya Cherkashina';
+
+const currentYear = '2022';
+let yourBirthYear = prompt('Ваш год рождения?');
+alert(`Ваш возраст: ${currentYear - yourBirthYear}`);
+
+let  sideLength = prompt('Укажите длину стороны квадрата:');
+alert(`Периметр квадрата равен: ${sideLength * 4}`); 
+
+let circleRadius = prompt('Укажите радиус круга:');
+alert(3.14 * (circleRadius * circleRadius));
---
+++ js-lesson_02/index.js
@@ -0,0 +1,108 @@
+// Task 1
+let name = 'Nataliya';
+console.log(name);
+
+// Task 2
+
+let number = '07';
+console.log(number);
+
+// Task 3
+
+let first = 'Добро ';
+let second = 'пожаловать ';
+let third = 'на курс';
+console.log(first, second, third);
+
+// Task 4
+
+//let year = '2022';
+//alert(year);
+
+// Task 5
+
+console.log(2019 - 200);
+
+// Task 6
+
+let six = document.querySelector('#six');
+six.innerHTML = 'Hello World'
+
+// Task 7
+
+let seven = document.querySelector('#seven');
+seven.innerHTML = 12 * 12;
+
+// Task 8
+
+let eight = document.querySelector('.out-8');
+eight.innerHTML = '&#x27;task-8&#x27;';
+
+// Task 9
+
+let nine = document.querySelector('.out-9');
+nine.innerHTML = 'world';
+
+// Task 10
+
+let ten = document.querySelector('.out-10');
+ten.innerHTML = '&#x3C;h2&#x3E;Hi&#x3C;/h2&#x3E;';
+
+// Task 11
+
+let out11 = document.querySelector('.out-11');
+out11.innerHTML = 123;
+out11.innerHTML += 456;
+
+// Task 12
+
+let a = document.querySelector('.out-12');
+a.innerHTML = 3.1415;
+
+// Task 13
+
+let out13 = document.querySelector('.out-13');
+out13.innerHTML = '&#x3C;img src=&#x22;https://itgid.info/img/js20/js20_unit_01.png&#x22; alt=&#x22;js&#x22;&#x3E;';
+
+// Task 14
+
+let z1 = 6;
+let z2 = 3;
+let out14 = document.querySelector('.out-14');
+out14.innerHTML = `${z1 * z2}`;
+
+// Task 15
+
+let y1 = 6;
+let y2 = 3;
+let out15 = document.querySelector('.out-15');
+out15.innerHTML = `${y1 / y2}`;
+
+// Task 16
+
+let x1 = 'Hello';
+let x2 = 5;
+let out16 = document.querySelector('.out-16');
+out16.innerHTML = `${x1 + x2}`;
+
+// Task 17
+
+let out17 = document.querySelector('.out-17');
+console.log(out17);
+
+// Task 18
+
+let out18 = document.querySelector('.out-18');
+console.log(out18);
+out18 = 5;
+console.log(out18);
+
+// Task 19
+
+let out19 = document.querySelector('.out-19');
+console.log(out19);
+
+
+// Task 20
+
+let out20 = document.querySelector('.out-20');
