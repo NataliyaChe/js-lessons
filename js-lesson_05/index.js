ae879f56b34c8c42db7cf8110e74efa6a2c8530b
NataliyaChe <happylibrarygeek@gmail.com>
2022-07-09 10:10:29 +0300

    Fix

---
+++ js-lesson_05/index.js
@@ -0,0 +1,11 @@
+function sum(a, b, c) {
+    return a + b + c;
+}
+// const result = sum(1, 2, 3);
+// console.log(result);
+
+console.log(sum(1, 2, 3));
+
+console.log(sum(4, 6, 89));
+
+console.log(sum());
--- js-lesson_05/training/index.js
+++ js-lesson_05/training/index.js
@@ -1,46 +1,106 @@
-// let inp = document.querySelector('.inp');
-// let word = inp.value;
-// let res = word.length;
-// let btn = document.querySelector('.btn');
-// let out = document.querySelector('.out');
-// console.log('test');
-// console.log(typeof inp);
-// console.log(typeof word);
-// console.log(typeof res);
-// btn.onclick = () => {
-//     console.log(res);
-// }
-
 // function getWordLength(word) { 
 //     console.log(word);
 //     return word.length;
 // }
-
 // // getWordLength('Hello')
 
 // console.log(getWordLength('Hello'));
 
+
 // function getWordLength(a, b) {
 //     return a.length === b.length;    
 // }
-// // console.log(getWordLength(a, b));
 // console.log(getWordLength('Hello', 'World'));
-let word = 'Hello';
-function charAt(word) {
-    // console.log(word = 'Hello');
-    // let word = 'Hello';
-    let result = String(' ');
-    console.log(typeof result);
-    console.log('result', result);
-    for (let i = word[0]; i <= word.length; i++) {
-        console.log(typeof i);
-        console.log(result);
-    }
-    // console.log(result);
-}
-charAt('Hello');
-console.log('test');
 
-// for (let char of "Hello") {
-//     console.log(char);
+
+// function charAt(word) {
+//     console.log('=====', word);
+//     for (let i = 0; i < word.length; i++) {
+//         console.log(word[i]);
+//     }
 // }
+// console.log(charAt('Hello'));
+
+// console.log(charAt('Natasha'));
+
+
+// function charAt(word) {
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         console.log(word[i]);
+//     }
+// }
+// console.log(charAt('Hello'));
+
+// console.log(charAt('Natasha'));
+
+
+// function charAt(word) {
+//     for (let i = 0; i < word.length; i++) {
+//         console.log(`${word[i]} - ${i + 1}`);
+//     }
+// }
+// console.log(charAt('Help'));
+
+
+// function getWinner(firstname, lastname) {
+//     const number = Math.floor(Math.random() * 1000) + 100;
+//     return `Наш победитель, ${firstname} ${lastname}, выиграл ${number} гривен`;
+// }
+// console.log(getWinner('Наталия', 'Черкашина'));
+
+
+// function getRandomNumber() {
+//     return Math.floor(Math.random() * 1000) + 100;
+// }
+// console.log(getRandomNumber());
+
+// function getWinner(firstname, lastname) {
+//      return `Наш победитель, ${firstname} ${lastname}, выиграл ${getRandomNumber()} гривен`;    
+// }
+// console.log(getWinner('Наталия', 'Черкашина'));
+
+
+// function rev(word) {
+//     let symb = '';
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         symb += word[i];
+//     }
+//     return symb;
+// }
+
+// console.log(rev('Hello'));
+
+
+// function rev(word) {
+//     let symb = '';
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         symb += word[i];
+//     }
+//     if  (symb === word) {
+//         return true;
+//     } else {
+//         return `Ваше слово ${word} не является палиндромом`;
+//     }
+// }
+// console.log(rev('lol'));
+
+
+// function rev(word) {
+//     let symb = '';
+//     for (let i = word.length - 1; i >= 0; i--) {
+//         symb += word[i];
+//     }
+//     return symb === word
+// }
+// console.log(rev('lol'));
+
+
+function rev(numb) {
+    let n = String(numb);
+    let symb = '';
+    for (let i = n.length - 1; i >= 0; i--) {
+        symb += n[i];
+    }
+    return symb === n;
+}
+console.log(rev(12321));
